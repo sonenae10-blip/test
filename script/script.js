51cb50db5f453855ce44a6a3021564604c25b973
sonenae10-blip <sonenae10@gmail.com>
2025-11-03 21:28:56 +0900

    script 완료

---
+++ script/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("#contactForm");
+  const message = document.querySelector("#formMessage");
+
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      message.classList.remove("hidden");
+      form.reset();
+
+      setTimeout(() => {
+        message.classList.add("hidden");
+      }, 3000);
+    });
+  }
+});
